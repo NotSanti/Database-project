2c5f694156509229abbcc858d210f57456b63fb8
Matteo <sdj00008@gmail.com>
2021-11-29 19:11:08 -0500

    Edited SQL creation class

--- InstallScript.sql
+++ InstallScript.sql
@@ -1,14 +1,47 @@
+DROP TABLE Components;
+DROP TABLE Distribution;
+DROP TABLE RolesConSong;
+DROP TABLE AuditLog;
+DROP TABLE RecordLabel;
+DROP TABLE Markets;
+DROP TABLE Contributors;
+DROP TABLE Song;
+DROP TABLE Roles;
+
+-- All time aspects will be in seconds --
+
 CREATE TABLE Roles (
     roleId VARCHAR2(20) PRIMARY KEY,
     roleName VARCHAR2(20)
-)
+);
 
 CREATE TABLE Contributors (
     contributorId VARCHAR2(20) PRIMARY KEY,
     fName VARCHAR(20),
     lName VARCHAR(20)
-)
+);
 
+CREATE TABLE Song (
+    songId VARCHAR2(20) PRIMARY KEY,
+    duration NUMBER(6)
+);
+
+CREATE TABLE Markets (
+    marketId VARCHAR(20) PRIMARY KEY,
+    area VARCHAR2(20)
+);
+
+CREATE TABLE RecordLabel (
+    recordLabelId VARCHAR2(20) PRIMARY KEY,
+    name VARCHAR2(20)
+);
+
+CREATE TABLE AuditLog (
+    userName VARCHAR2(20) PRIMARY KEY,
+    dateInfo Date,
+    information VARCHAR2(100)
+);
+    
 CREATE TABLE RolesConSong (
     contributorId VARCHAR(20),
     roleId VARCHAR2(20),
@@ -17,13 +50,8 @@ CREATE TABLE RolesConSong (
     FOREIGN KEY (songId) REFERENCES Song(songId),
     FOREIGN KEY (contributorId) REFERENCES Contributors(contributorId),
     PRIMARY KEY(contributorId,roleId,songId)
-)
+);
 
-CREATE TABLE Song (
-    songId VARCHAR2(20) PRIMARY KEY,
-    duration NUMBER(6)
-    -- duration in seconds
-)
 
 CREATE TABLE Components (
     componentId VARCHAR(20) PRIMARY KEY,
@@ -34,7 +62,7 @@ CREATE TABLE Components (
     offsetSong NUMBER(10),
     durationSong NUMBER(10),
     FOREIGN KEY (songId) REFERENCES Song(songId)
-)
+);
 
 CREATE TABLE Distribution (
     songId VARCHAR(20),
@@ -44,24 +72,9 @@ CREATE TABLE Distribution (
     FOREIGN KEY (songId) REFERENCES Song(songId),
     FOREIGN KEY (recordLabelId) REFERENCES RecordLabel(recordLabelId),
     FOREIGN KEY (marketId) REFERENCES Markets(marketId)
-)
+);
 
-CREATE TABLE Markets (
-    marketId VARCHAR(20) PRIMARY KEY,
-    area VARCHAR2(20)
-)
 
-CREATE TABLE RecordLabel (
-    recordLabelId VARCHAR2(20) PRIMARY KEY,
-    name VARCHAR2(20)
-)
-
-CREATE TABLE AuditLog (
-    userName VARCHAR2(20) PRIMARY KEY,
-    dateInfo Date,
-    information VARCHAR2(100)
-)
-    
     
     
     
