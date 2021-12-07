04a4d03ace26843c7bbb6d56805785d38b551b55
Matteo <sdj00008@gmail.com>
2021-12-06 19:24:26 -0500

    Added pl/sql file

---
+++ Project.sql
@@ -0,0 +1,10 @@
+-- Question #1 (Santiago)
+-- Show all contributos and all their roles for a givin recording/compilation
+
+
+-- Question #2 (Matteo)
+-- Show all details of a recording/compilation
+
+
+-- Question #3 (Oleks)
+-- show all recoridng/compilatiosn a contributor may have been in and their role
--- Song.java
+++ Song.java
@@ -3,11 +3,15 @@ import java.util.List;
 public class Song {
     protected String songId;
     protected int duration;
+    protected List<Contributors> contributors;
+    protected List<Roles> roles;
     protected List<Components> components;
     
-    public Song(String songId, int duration, List<Components> components) {
+    public Song(String songId, int duration, List<Contributors> contributors, List<Roles> roles, List<Components> components) {
         this.songId = songId;
         this.duration = duration;
+        this.contributors = contributors;
+        this.roles = roles;
         this.components = components;
     }
 }
